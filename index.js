cc059902bca30f16e10edc0b4b0b41fc837c4e55
Tanawat Yodnil <littleboycoding@gmail.com>
2025-11-03 01:06:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.end("Hello, World, C");
+	res.end("Hello, World");
 });
 
 const PORT = process.env.PORT || 8080
